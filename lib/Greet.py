358224460442c74991c7fe89e5ef40acc45bc31d
TJ Tyrrell <innovate@tjtyrrell.com>
2019-02-20 22:36:30 -0500

    master: Check version to decide if the changelog should be shown

--- lib/Greet.py
+++ lib/Greet.py
@@ -5,12 +5,12 @@ class Greet():
 
 	def display_changelog():
 
-		current_version = Sdf.Settings.get_setting("plugin_version", {})
-		print( current_version )
-		# changelog_url = "https://raw.githubusercontent.com/limebox/sublime-sdf/master/CHANGELOG.md"
-		# response = urllib.request.urlopen( download_url )
-		# #response = urllib.urlopen( download_url )
-		# changelog_info = response.read()
+		current_version = Settings.get_setting("plugin_version", {})
+		if current_version != plugin_version:
+			changelog_url = "https://raw.githubusercontent.com/limebox/sublime-sdf/master/CHANGELOG.md"
+			response = urllib.request.urlopen( download_url )
+			#response = urllib.urlopen( download_url )
+			changelog_info = response.read()
 
-		# view = sublime.Window.new_file( sublime.active_window() )
-		# view.run_command("insert", {"characters": changelog_info})
+			view = sublime.Window.new_file( sublime.active_window() )
+			view.run_command("insert", {"characters": changelog_info})
